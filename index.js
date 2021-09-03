60667938cf09fc8fef1d554e3562ebf6c40275d7
rachelperez <rlb2rlp@gmail.com>
2021-09-02 20:34:30 -0700

    finish styling, add fun button animations, correct sizing to accomodate 13 inch screen

--- index.js
+++ index.js
@@ -19,24 +19,71 @@
 // ==> JS for word-submission.html <==
 const madlib1 = {
     title: "The Job Interview from Hell",
-    content: `I scoured my closet for the perfect ____ to wear. Dressing for the ____ place around isn't easy. Sweating _____ during the interview is a sure sign of weakness. I must remember to play up my strengths: ____, menacing stares and ____ collecting. It has always been my dream to work with the Devil. The power and prestige are extremely desirable.The Gates of ____ tower above me as I ring the doorbell. I can't believe ____ is the doorbell chime. My goodness, his red ____ look(s) better in person!
+    content: `I scoured my closet for the perfect ____ to wear.
+
+    Dressing for the ____ place around isn't easy. 
+
+    Sweating ____ during the interview is a sure sign of weakness. 
+
+    I must remember to play up my strengths: ____, menacing stares and ____ collecting. 
+
+    It has always been my dream to work with the Devil. The power and prestige are extremely desirable.
+
+    The Gates of ____ tower above me as I ring the doorbell. 
+
+    I can't believe ____ is the doorbell chime. My goodness, his red ____ look(s) better in person!
+
     The Devil: "Welcome to Hell. Your inverview will ____ now."
+
     Me: "It's a pleasure to meet you. My name is ____."
+
     The Devil: "Who cares? I have only one question. Do you hate ____?"
+
     Me: "Yes, yes I do. I hate them more than ____."
+
     The Devil: "You're hired. Wait, is that a ____ you're wearing?"
+
     Me: "No, it's actually a ____ suit."
+
     The Devil: "You're fired. Maybe you should pursure a career in ____."`,
     words: ["article of clothing", "superlative", "plural noun", "sport", "noun", "proper noun", "song title", "article of clothing", "verb", "your name", "plural noun", "plural noun", "article of clothing", "adjective", "field of study"]
 };
 const madlib2 = {
     title: "Generic Superhero Movie",
-    content: `Meet our hero ____, a super-intelligent ____. A run-in with the ____ military leads him to create his alter-ego ____, a ____ ____ giant capable of great destruction. He battles the military with his girlfriend Jessica. Eventually it is discovered that our hero's long-time colleague ____, distinguished by his ____ ____, is trying to turn our hero into a weapon. This leads to a climactic (if pointless) battle in downtown ____ with an evil version of the same giant alter-ego called The Diabolical ____. Eventually the enemy is subdued by ____ him with a ____ and the city is saved.`,
+    content: `Meet our hero ____, a super-intelligent ____.
+
+    A run-in with the ____ military leads him to create his alter-ego ____, 
+    
+    a ____ ____ giant capable of great destruction.
+    
+    He battles the military with his girlfriend Jessica.
+    
+    Eventually it is discovered that our hero's long-time colleague ____, distinguished by his ____ ____, is trying to turn our hero into a weapon.
+    
+    This leads to a climactic (if pointless) battle in downtown ____ with an evil version of the same giant alter-ego called The Diabolical ____.
+    
+    Eventually the enemy is subdued by ____ him with a ____ and the city is saved.`,
     words: ["silly name", "unrealistic profession", "country", "another silly name", "color", "adjective", "third silly name", "adjective", "facial feature", "US city", "one more silly name", "-ing verb", "noun"]
 };
 const madlib3 = {
     title: "What's for Dinner?",
-    content: `It was Thanksgiving, and the scent of succulent roast ____ wafted through my house. "It's time to ____!" my mother called. I couldn't wait to get my ____ on that ____ Thanksgiving meal. My family sat around the dining-room ____. It was laid out with every kind of ____ imaginable. There was a basked of hot buttered ____ and glasses of sparkling ____. The ____ turkey sat, steaming, next to a tureen of ____ gravy. A bowl of ruby-red ____ sauce, a sweet-____ casserole, and a dish of mashed ____ tempted my taste buds. But the dish I looked forward to most was Grandma ____'s famous ____ pie. Thanksgiving is my favorite holiday, ____ down.`,
+    content: `It was Thanksgiving, and the scent of succulent roast ____ wafted through my house.
+    
+    "It's time to ____!" my mother called.
+    
+    I couldn't wait to get my ____ on that ____ Thanksgiving meal.
+    
+    My family sat around the dining-room ____.
+    
+    It was laid out with every kind of ____ imaginable.
+    
+    There was a basked of hot buttered ____ and glasses of sparkling ____.
+    
+    The ____ turkey sat, steaming, next to a tureen of ____ gravy.
+    
+    A bowl of ruby-red ____ sauce, a sweet-____ casserole, and a dish of mashed ____ tempted my taste buds.
+    
+    But the dish I looked forward to most was Grandma ____'s famous ____ pie. Thanksgiving is my favorite holiday, ____ down.`,
     words: ["noun", "verb", "part of body (plural)", "adjective", "noun", "noun", "plural noun", "type of liquid", "adjective", "noun", "noun", "noun", "plural noun", "person in zoom", "noun", "part of body (plural)"]
 };
 const form = document.querySelector("form");
@@ -47,6 +94,7 @@ const inputContainer = document.querySelector("#input-container")
 const storyContainer = document.querySelector("#story-container");
 const allInputs = document.getElementsByTagName("input");
 const story = document.querySelector("#giflib-paragraph")
+const nextButton = document.querySelector('#next-button');
 let allInputValues = [];
 let giflib = {};
 const availMadlibs = [madlib1, madlib2, madlib3];
@@ -54,32 +102,34 @@ const availMadlibs = [madlib1, madlib2, madlib3];
 // Create inputs and attach to form element
 function renderStory() {
     story.innerText = giflib.content;
-    storyContainer.append(story);
+    storyContainer.append(story, startButton);
 };
 
 function createInputs() {
+    storyContainer.innerHTML = "";
     giflib = availMadlibs.shift();
     giflib.words.forEach((word) => {
         const newInput = document.createElement("input");
         newInput.type = "text";
         newInput.className = "word-input";
-        newInput.value = "";
         newInput.placeholder = word;
-        newInput.required = true;
         form.append(newInput)
     })
-    form.append(submitButton);
-    storyTitle.innerText = giflib.title
-    renderStory();
+    storyTitle.innerText = giflib.title;
+    submitButton.style.display = "block";
 };
 
 // Collect array of input values to run through fetchGifs() and hide form
+// remove whitespace from input values
 
 function inputValueArray() {
     Array.prototype.forEach.call(allInputs, input => {
-        allInputValues.push(input.value);
+        const inputValue = encodeURIComponent(input.value);
+        allInputValues.push(inputValue);
     });
-    form.style.display = "none";
+    console.log(allInputValues)
+    form.innerHTML = "";
+    submitButton.style.display = "none";
 }
 
 function fetchGifs(array) {
@@ -93,11 +143,18 @@ function fetchGifs(array) {
 // Add gifs to madlib object and element.replace("____", gif) in order from top to bottom of the story
 function renderGifs(embedArray) {
     embedArray.forEach((embedUrl) => {
-        story.innerHTML = story.innerHTML.replace("____", `<iframe src=${embedUrl} class="giphy-link"></iframe>`);
-        storyContainer.style.display = "flex";
+        story.innerHTML = story.innerHTML.replace("____", `<iframe src=${embedUrl} class="giphy-link" align="middle"></iframe>`);
 })
 }
-//Event listner
+
+//Scroll to top function for next btn
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+//Event listner "LETS DO THIS"
 submitButton.addEventListener("click", (event) => {
     event.preventDefault();
     inputValueArray();
@@ -106,7 +163,14 @@ submitButton.addEventListener("click", (event) => {
         const embeds = responses.map((response) => {
             return response.data.data.embed_url
         });
+        renderStory();
         renderGifs(embeds);
-        console.log(story.innerHTML)
     }).catch(err =>console.error(err))
+    nextButton.style.display = "block"
     });
+
+// Event Listener NEXT LIB
+nextButton.addEventListener('click', () => {
+    scrollToTop();
+    createInputs();
+})
